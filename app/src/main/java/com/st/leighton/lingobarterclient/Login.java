699c59bd14a26798e11cb0b65b10f6ba4c31ecd2
Haoxiang <hx2185@columbia.edu>
2016-05-06 07:58:08 -0400

    Notice: --Add logo image resource: res/drawable --Modified the manifest to no-action-bar style

--- app/src/main/java/com/st/leighton/lingobarterclient/Login.java
+++ app/src/main/java/com/st/leighton/lingobarterclient/Login.java
@@ -2,12 +2,20 @@ package com.st.leighton.lingobarterclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        this.setContentView(R.layout.activity_login);
     }
 }
