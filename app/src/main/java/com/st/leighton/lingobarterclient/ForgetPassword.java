a259b66b4612921b191a67a57ade07b1241a871e
Haoxiang <hx2185@columbia.edu>
2016-05-08 00:19:06 -0400

    Login/Register basic UI finished

--- app/src/main/java/com/st/leighton/lingobarterclient/ForgetPassword.java
+++ app/src/main/java/com/st/leighton/lingobarterclient/ForgetPassword.java
@@ -1,13 +1,22 @@
 package com.st.leighton.lingobarterclient;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ForgetPassword extends AppCompatActivity {
+
+    Button loginB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+        loginB = (Button) findViewById(R.id.hx_forget_button_login);
+        if (loginB != null) {
+            loginB.setPaintFlags(loginB.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        }
     }
 }
