66e76856983279feb76db0da9ae28354c1897858
Haoxiang <hx2185@columbia.edu>
2016-05-07 22:34:21 -0400

    Login finished (service required)

--- app/src/main/java/com/st/leighton/lingobarterclient/Login.java
+++ app/src/main/java/com/st/leighton/lingobarterclient/Login.java
@@ -1,44 +1,91 @@
 package com.st.leighton.lingobarterclient;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Paint;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    Context baseContext;
+
+    Button logginB;
+    Button forgetPasswordB;
+    Button registerB;
+
+    EditText emailET;
+    EditText passwordET;
+
+    String email = "", password = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
-
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_login);
 
-        Button registerBtn = (Button) findViewById(R.id.hx_login_button_register);
-        Button retrieveBtn = (Button) findViewById(R.id.hx_login_button_forget_password);
-        if (registerBtn != null) {
-            registerBtn.setPaintFlags(registerBtn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-        }
-        if (retrieveBtn != null) {
-            retrieveBtn.setPaintFlags(retrieveBtn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-        }
+        baseContext = this;
+
+        logginB = (Button) findViewById(R.id.hx_login_button_login);
+        forgetPasswordB = (Button) findViewById(R.id.hx_login_button_forget_password);
+        registerB = (Button) findViewById(R.id.hx_login_button_register);
+
+        emailET = (EditText) findViewById(R.id.hx_login_edit_email);
+        passwordET = (EditText) findViewById(R.id.hx_login_edit_password);
+
+        forgetPasswordB.setPaintFlags(forgetPasswordB.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        registerB.setPaintFlags(registerB.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        logginB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetBackgroundColors();
+                email = emailET.getText().toString();
+                password = passwordET.getText().toString();
+
+                if (email.equals("")) {
+                    Toast.makeText(baseContext,"Please input email", Toast.LENGTH_LONG).show();
+                    emailET.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.colorAccent));
+                    return ;
+                }
+
+                if (password.equals("")) {
+                    Toast.makeText(baseContext,"Please input password", Toast.LENGTH_LONG).show();
+                    passwordET.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.colorAccent));
+                    return ;
+                }
+
+                Intent intent = new Intent(baseContext, LanguageTalks.class);
+                startActivity(intent);
+            }
+        });
+
+        forgetPasswordB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetBackgroundColors();
+                Intent intent = new Intent(baseContext, ForgetPassword.class);
+                startActivity(intent);
+            }
+        });
+
+        registerB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetBackgroundColors();
+                Intent intent = new Intent(baseContext, Register.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void register(View view) {
-        Intent intent = new Intent(this, Register.class);
-        startActivity(intent);
-    }
-
-    public void check_identity(View view) {
-        Intent intent = new Intent(this, LanguageTalks.class);
-        startActivity(intent);
+    void resetBackgroundColors() {
+        emailET.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.DimGray));
+        passwordET.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.DimGray));
     }
 }
