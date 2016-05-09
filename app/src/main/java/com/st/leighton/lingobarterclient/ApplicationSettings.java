a7a5a3c5769e547961e93f0a3f794c592827e034
Haoxiang <hx2185@columbia.edu>
2016-05-09 11:01:15 -0400

    Finished Self Information UI

--- app/src/main/java/com/st/leighton/lingobarterclient/ApplicationSettings.java
+++ app/src/main/java/com/st/leighton/lingobarterclient/ApplicationSettings.java
@@ -3,7 +3,6 @@ package com.st.leighton.lingobarterclient;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.support.annotation.IntegerRes;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
