52c27208cc425f8a9981257d044c914d95907035
Haoxiang <hx2185@columbia.edu>
2016-05-09 07:42:13 -0400

    add change password option (fixed)

--- app/src/main/java/com/st/leighton/lingobarterclient/ChangePassword.java
+++ app/src/main/java/com/st/leighton/lingobarterclient/ChangePassword.java
@@ -1,7 +1,6 @@
 package com.st.leighton.lingobarterclient;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Paint;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
